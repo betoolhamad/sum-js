73364430bc7266bac13faf483c37af2394a5e6de
Betool Hamad <43522153+betoolhamad@users.noreply.github.com>
2024-12-23 11:59:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ var result=()=>{
    let inp=parseInt(document.getElementById("input1").value);
    let inp2=parseInt(document.getElementById("input2").value);
    let res=document.getElementById("result");
+    if (isNaN(inp) || isNaN(inp2)) {
+    res.value = "الرجاء إدخال أرقام صحيحة.";
+    return;
+    }
    let result = sum(inp,inp2);
    res.value=result;
 }
